cadb72a9741220075804d55628c9c00fb6328b96
Asma22334585 <guesmihadil0@gmail.com>
2024-04-21 03:45:37 -0700

    my commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" starts a Flask web application:"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def d_route():
+    """default_route"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """hbnb"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cfun(text):
+    """c what"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text='is cool'):
+    """python is cool"""
+    return "Python {}".format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
